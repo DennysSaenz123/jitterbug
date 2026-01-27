b3698a3ac087b1f92417cad5ec47abcf3708706c
IQUER_SAENZ <206500499+DennysSaenz123@users.noreply.github.com>
2026-01-27 19:01:09 +0000

    Added bug sprite to game

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_sprite_ptr.h>
+
+
+#include "bn_sprite_items_ladybug.h"
 
 int main(){
     bn::core::init();
 
     bn::backdrop::set_color(bn::color(31, 20, 5));
+
+    bn::sprite_ptr ladybug = bn::sprite_items::ladybug.create_sprite();
 
     while (true)
     {
